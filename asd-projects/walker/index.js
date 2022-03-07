b209bd1d541c48cd87729210fac5412b58941064
bhadiememe <meianiblack74@gmail.com>
2022-03-07 19:16:51 +0000

    saving image filtering

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,13 +10,18 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+  var KEY = {
+    "LEFT": 37,
+    "UP": 38,
+    "RIGHT": 39,
+    "DOWN": 40,
+  };
   // Game Item Objects
 
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -34,8 +39,17 @@ function runProgram(){
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+    if (event.which === KEY.UP) {
+      console.log("up pressed");
+    }else if(event.which === KEY.DOWN){
+      console.log("down pressed");
+    }
+    {console.log("right pressed");
+}
+{
+console.log("left pressed");
+}
   }
 
   ////////////////////////////////////////////////////////////////////////////////
